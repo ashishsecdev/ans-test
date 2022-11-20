a2ee36e3157cd335eccc0e6aa656540947a818bb
Arnold Johnson <118655159+arnoldj-devops@users.noreply.github.com>
2022-11-20 13:36:01 +0530

    add: template rendering

--- GCP-instance-create.tf
+++ GCP-instance-create.tf
@@ -1,3 +1,11 @@
+data "template_file" "script_file" {
+  template = file("m_test.sh")
+  vars = {
+    eip1 = google_compute_instance.node1.network_interface.0.access_config.0.nat_ip
+    eip2 = google_compute_instance.node1.network_interface.0.access_config.0.nat_ip
+  }
+}
+
 resource "google_compute_instance" "master" {
   name = "ansible-master"
   machine_type = var.master_instance
@@ -14,7 +22,11 @@ resource "google_compute_instance" "master" {
     access_config {
     }
   }
-  metadata_startup_script = file("${path.module}/m_test.sh")
+  metadata_startup_script = data.template_file.script_file.rendered
+  depends_on = [
+    google_compute_instance.node1,
+    google_compute_instance.node2,
+  ]
 }
 
 resource "google_compute_instance" "node1" {
